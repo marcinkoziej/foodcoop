f604ed2a23e1113c9dfe2f27974552cdabfbb477
krip <Kripek@e180080149.adsl.alicedsl.de>
2011-08-22 23:21:46 +0200

    jakies zmiany w modelu

--- application/config/database.php
+++ application/config/database.php
@@ -38,7 +38,7 @@
 | the active record class
 */
 
-$active_group = 'default';
+$active_group = 'postgre';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
--- application/models/user.php
+++ application/models/user.php
@@ -5,6 +5,27 @@ define('COORDINATOR_USER', '1');
 define('ADMIN_USER', '2');
 
 class User extends CI_Model {
+  public static $fields = array(
+				/*				'user_id' => array(
+                                                 'type' => 'INT',
+                                                 'constraint' => 5, 
+                                                 'unsigned' => TRUE,
+                                                 'auto_increment' => TRUE
+						 ),*/
+				'username' => array(
+						    'type' => 'VARCHAR',
+						    'constraint' => '100',
+						    ),
+				'password' => array(
+						    'type' =>'VARCHAR',
+						    'constraint' => '100',
+                                          ),
+				'coop' => array(
+						'type' => 'INT',
+                                                 'null' => FALSE,
+							    ),
+				);
+
   var $username = '';
   var $password = '';
   var $coop = null;
